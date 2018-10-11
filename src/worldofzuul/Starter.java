8facf011c6babdfbb8cc0afd14fe90ada23b8449
FrederikBRoth <32262672+FrederikBRoth@users.noreply.github.com>
2018-10-11 10:06:37 +0200

    Added Netbeans Project

---
+++ src/worldofzuul/Starter.java
@@ -0,0 +1,18 @@
+
+package worldofzuul;
+
+/**
+ *
+ * @author fredd
+ */
+public class Starter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+    
+}
