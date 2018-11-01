accac354b4d5bc8d4588c6ee3bec6727adf3c374
ImTruxaHD <imtruxa@gmail.com>
2018-11-01 22:06:25 +0100

    bug fixing
    fixed bug in map gen

--- src/worldofzuul/Map.java
+++ src/worldofzuul/Map.java
@@ -20,7 +20,7 @@ public class Map {
     Room jailRoom3 = new Room("14");
     Room roomRoom3 = new Room("15");
     ArrayList<Room> roomList = new ArrayList<>();
-    
+
     private Room startingRoom;
 
     public Map(int levelSize) {
@@ -51,30 +51,42 @@ public class Map {
         grid = genNeighbours(grid, startingRoomPos, startingRoomPos);
 
         //Iterates through 2 dimensional array "grid", and generates rooms with genNeighbours method
-        while(roomList.size() > 0){
+        while (roomList.size() > 0) {
             for (int y = 0; y < grid.length; y++) {
-            for (int x = 0; x < grid[y].length; x++) {
-                if (grid[y][x] != null) {
-                    genNeighbours(grid, y, x);
-                }
-                if (roomList.size() <= 0) {
-                    break;
-                }
-            }
+                for (int x = 0; x < grid[y].length; x++) {
+                    if (grid[y][x] != null && Math.random() < 0.75) {
+                        grid = genNeighbours(grid, x, y);
 
-            if (roomList.size() <= 0) {
-                break;
+                        //Prints a visualisation of the map in grid form
+                        for (int y1 = 0; y1 < grid.length; y1++) {
+                            for (int x1 = 0; x1 < grid[y1].length; x1++) {
+                                if (grid[y1][x1] != null) {
+                                    System.out.print(" " + grid[y1][x1]);
+                                }
+                                System.out.print(" \t");
+                            }
+                            System.out.println("");
+                        }
+
+                        if (roomList.size() <= 0) {
+                            break;
+                        }
+                    }
+                    if (roomList.size() <= 0) {
+                        break;
+                    }
+                }
+
             }
         }
-        }
-        
+
         //Iterate through the map, setting up exits
         for (int y = 0; y < grid.length; y++) {
-            
+
             for (int x = 0; x < grid[y].length; x++) {
-                
+
                 //Check for location north of room, if a room exsists there, and links up the room with an exit/entrance if it does
-                 if (y - 1 >= 0 && grid[y - 1][x] != null && grid[y][x] != null) {
+                if (y - 1 >= 0 && grid[y - 1][x] != null && grid[y][x] != null) {
                     grid[y][x].setExit("north", grid[y - 1][x]);
                 }
                 //Check for location west of room, if a room exsists there, and links up the room with an exit/entrance if it does
@@ -109,36 +121,36 @@ public class Map {
 
         //Check north, if no room exists, create random room
         //If room exists, create no room
-        if (y - 1 >= 0 && map[y - 1][x] == null && Math.random() < 0.75) {
+        if (y - 1 >= 0 && map[y - 1][x] == null && roomList.size() > 0) {
             int roomIndex = (int) (Math.random() * roomList.size());
             map[y - 1][x] = roomList.get(roomIndex);
             roomList.remove(roomIndex);
         }
         //Check west, if no room exists, create random room
         //If room exists, create no room
-        if (x - 1 >= 0 && map[y][x - 1] == null && Math.random() < 0.75) {
+        if (x - 1 >= 0 && map[y][x - 1] == null && roomList.size() > 0) {
             int roomIndex = (int) (Math.random() * roomList.size());
             map[y][x - 1] = roomList.get(roomIndex);
             roomList.remove(roomIndex);
         }
         //Check east, if no room exists, create random room
         //If room exists, create no room
-        if (x + 1 < map[y].length && map[y][x + 1] == null && Math.random() < 0.75) {
+        if (x + 1 < map[y].length && map[y][x + 1] == null && roomList.size() > 0) {
             int roomIndex = (int) (Math.random() * roomList.size());
             map[y][x + 1] = roomList.get(roomIndex);
             roomList.remove(roomIndex);
         }
         //Check south, if no room exists, create random room
         //If room exists, create no room
-        if (y + 1 < map.length && map[y + 1][x] == null && Math.random() < 0.75) {
+        if (y + 1 < map.length && map[y + 1][x] == null && roomList.size() > 0) {
             int roomIndex = (int) (Math.random() * roomList.size());
             map[y + 1][x] = roomList.get(roomIndex);
             roomList.remove(roomIndex);
         }
         return map;
     }
-    
-    public Room getStartingRoom(){
+
+    public Room getStartingRoom() {
         return this.startingRoom;
     }
 
