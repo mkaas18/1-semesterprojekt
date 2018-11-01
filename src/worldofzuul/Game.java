2541a326c7b0d9082c34d66ea0ccd1d20f81c5c1
FrederikBRoth <32262672+FrederikBRoth@users.noreply.github.com>
2018-11-01 19:11:23 +0100

    Items_Questions_MapGen

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -8,18 +8,13 @@ public class Game
     private Question question = new Question();
         
 
-<<<<<<< HEAD
-    public Game() {
-        createRooms();
-        parser = new Parser();
-        createItems();
-=======
+
     public Game() 
     {
         parser = new Parser();
         Map map = new Map(7);
         currentRoom = map.getStartingRoom();
->>>>>>> MapGen_Test
+        createItems();
     }
 
     public void createItems() {
@@ -37,34 +32,6 @@ public class Game
         }
 
     }
-
-<<<<<<< HEAD
-    private void createRooms() {
-        Room outside, theatre, pub, lab, office;
-
-        outside = new Room("outside the main entrance of the university");
-        theatre = new Room("in a lecture theatre");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-
-        outside.setExit("east", theatre);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
-
-        theatre.setExit("west", outside);
-
-        pub.setExit("east", outside);
-
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
-
-        office.setExit("west", lab);
-
-        currentRoom = outside;
-    }
-=======
->>>>>>> MapGen_Test
 
     public void play() {
         printWelcome();
