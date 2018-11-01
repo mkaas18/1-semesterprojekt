b4c5b816f0fb719ae16e3e781bd7a94f1b9a1d04
Qadaar <43170853+Qadaar@users.noreply.github.com>
2018-11-01 15:11:55 +0100

    Vi har lavet statsne til subklasser af klassen Player derfra tilføjet getter og setter og bestemmer ogs vi ind i metoderne imorgen.

--- src/worldofzuul/Player.java
+++ src/worldofzuul/Player.java
@@ -10,58 +10,72 @@ package worldofzuul;
  */
 public class Player {
     private String name;
+    private int maxHP = 100;
+    private int endurance = 0;
+    private int strength = 0;
+    private int agility = 0;
+    private int intellect = 0;
+    private int damagetaken = 0;
     
-    
-    // Jeg ved ikke om det skal være private fields eller måske public fields
-    public int hp, hpDecrease, hpIncrease, maxHP;   
-// Constructor for navnet på playeren
-    public Player(String name){
+    public Player(String name, int maxHP, int endurance, int strength, int agility, int intellect, int damagetaken){
         this.name = name;
-    }
-    // Vi laver ny klasse for hver stats. derfra henter vi statsne i den enkelte klasse.
-
-public Player(int hp, int hpIncrease, int hpDecrease, int maxHP) {
-        this.hp = hp; 
-        this.hpIncrease = hpIncrease;
-        this.hpDecrease= hpDecrease;
-        this.maxHP = maxHP;     
-    }
-    
- public void setHp(int hp) {
-        this.hp = hp;
-    }
-
-    public void setHpDecrease(int hpDecrease) {
-        this.hpDecrease = hpDecrease;
-    }
-
-    public void setHpIncrease(int hpIncrease) {
-        this.hpIncrease = hpIncrease;
-    }
-
-    public void setMaxHP(int maxHP) {
         this.maxHP = maxHP;
+        this.endurance = endurance;
+        this.strength = strength;
+        this.agility = agility;
+        this.intellect = intellect;
+        this.damagetaken = damagetaken;
     }
-    public int getHP(){
-        return hp;
-    }
-
-    public int getHpDecrease() {
-        return hpDecrease;
-    }
-
-    public int getHpIncrease() {
-        return hpIncrease;
+    public String getName(String name){
+    return this.name;
     }
 
-    public int getMaxHP() {
-        return maxHP;
+    public int getMaxHP(){
+    return this.maxHP;
     }
 
-   
+    public void addmaxHP(int amount){
+    this.maxHP += amount;
+    }
+    public int damagetaken(){
+        return this.damagetaken;
+    }
     
+    public int adddamagetaken(int amount){
+        return this.maxHP -= amount;
     
-}
+    }
+    public int getEndurance(){
+    return this.endurance;
+    }
+
+    public void addEndurance(int amount){
+    this.endurance += amount;
+    }
+    public int getStrength(){
+    return this.strength;
+    }
+
+    public void strength(int amount){
+    this.strength += amount;
+    }
+    public int getAgility(){
+    return this.agility;
+    }
+
+    public void addAgility(int amount){
+    this.agility += amount;
+    }
+    public int getIntellect(){
+    return this.intellect;
+    }
+
+    public void addIntellect(int amount){
+    this.intellect += amount;
+    }
+    }
+    
+  
     
     
     
