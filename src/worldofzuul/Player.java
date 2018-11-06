3babd15529ebb46eff21a2041ffe38c1096db3e6
Mikkel Høyberg <mikkelhoyberg@gmail.com>
2018-11-06 16:05:18 +0100

    Merge player and items
    merge

--- src/worldofzuul/Player.java
+++ src/worldofzuul/Player.java
@@ -1,88 +1,74 @@
-/*
- make le items le Mikkelo
-
- */
 package worldofzuul;
 
-/**
- *
- * @author Karla
- */
 public class Player {
+
     private String name;
     private int maxHP = 100;
-    private int endurance = 0;
-    private int strength = 0;
-    private int agility = 0;
-    private int intellect = 0;
     private int damagetaken = 0;
-    
-    public Player(String name, int maxHP, int endurance, int strength, int agility, int intellect, int damagetaken){
+    private Stats stats;
+
+    public Player(String name, int maxHP, int damagetaken) {
         this.name = name;
         this.maxHP = maxHP;
-        this.endurance = endurance;
-        this.strength = strength;
-        this.agility = agility;
-        this.intellect = intellect;
+        this.stats = new Stats();
         this.damagetaken = damagetaken;
     }
-    public String getName(String name){
-    return this.name;
+
+    public String getName(String name) {
+        return this.name;
     }
 
-    public int getMaxHP(){
-    return this.maxHP;
+    public int getMaxHP() {
+        return this.maxHP;
     }
 
-    public void addmaxHP(int amount){
-    this.maxHP += amount;
+    public void addmaxHP(int amount) {
+        this.maxHP += amount;
     }
-    public int damagetaken(){
+
+    public int damagetaken() {
         return this.damagetaken;
     }
-    
-    public int adddamagetaken(int amount){
+
+    public int adddamagetaken(int amount) {
         return this.maxHP -= amount;
-    
-    }
-    public int getEndurance(){
-    return this.endurance;
+
     }
 
-    public void addEndurance(int amount){
-    this.endurance += amount;
-    }
-    public int getStrength(){
-    return this.strength;
+    public int getEndurance() {
+        return this.stats.getEndurance();
     }
 
-    public void strength(int amount){
-    this.strength += amount;
-    }
-    public int getAgility(){
-    return this.agility;
+    public void addEndurance(int amount) {
+        this.stats.setEndurance(this.stats.getEndurance() + amount);
     }
 
-    public void addAgility(int amount){
-    this.agility += amount;
-    }
-    public int getIntellect(){
-    return this.intellect;
+    public int getStrength() {
+        return this.stats.getStrength();
     }
 
-    public void addIntellect(int amount){
-    this.intellect += amount;
+    public void strength(int amount) {
+        this.stats.setStrength(this.stats.getStrength() + amount);
     }
+
+    public int getAgility() {
+        return this.stats.getAgility();
     }
-    
-  
-    
-    
-    
-    
-    //Methods for øge og minke de forskellige variabler 
-    // Ikke kommet dertil endnu lel
-    // Og de forskellige ting, stats skal lave kunne ?? 
-      
-    
-    
+
+    public void addAgility(int amount) {
+        this.stats.setAgility(this.stats.getAgility() + amount);
+    }
+
+    public int getIntellect() {
+        return this.stats.getIntelligence();
+    }
+
+    public void addIntellect(int amount) {
+        this.stats.setIntelligence(this.stats.getIntelligence() + amount);
+    }
+}
+
+//Methods for øge og minke de forskellige variabler 
+// Ikke kommet dertil endnu lel
+// Og de forskellige ting, stats skal lave kunne ?? 
+
