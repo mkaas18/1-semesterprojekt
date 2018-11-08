0a3c39740b7822c93afc3facc8c1b1b15d39fc31
FrederikBRoth <32262672+FrederikBRoth@users.noreply.github.com>
2018-11-08 23:20:45 +0100

    Adding more shop shit

--- src/worldofzuul/Shop.java
+++ src/worldofzuul/Shop.java
@@ -5,6 +5,7 @@
  */
 package worldofzuul;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,9 +13,44 @@ import java.util.Scanner;
  * @author fredd
  */
 public class Shop {
+    ArrayList<Item> buyable;
     ItemGenerator itemGen;
+    Item dummyItem = new Item();
     Scanner input = new Scanner(System.in);
     
-    Shop(){   
+    Shop(){
+        buyable.add(itemGen.generateItem(1));
+        buyable.add(itemGen.generateItem(1));
+        buyable.add(itemGen.generateItem(1));
     }
+    
+    public void startShop(Player player){
+        System.out.println("You have arrived at a shop\n"
+                + "Would you like to take a look?\nYes/No");
+        while(true){
+            if(input.next().toLowerCase().equals("yes")){
+                System.out.println("Feel free to browse!");
+                while(true){
+                    int i = 1;
+                    for(Item item : buyable){
+                        System.out.println(item.getName() + "(" + i + ")");
+                        i++;
+                    }
+                    System.out.println("Please choose an item based on their numbering");
+                    try {
+
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                    }
+                }
+                
+                
+            } else if(input.next().toLowerCase().equals("no")){
+                System.out.println("Alright, have a good day!");
+                break;
+            } else {
+                System.out.println("What do you mean?");
+            }
+        }
+    }
+    
 }
